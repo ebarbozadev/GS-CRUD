9b1e019f4adc856392aee1e7752e24009f03a3bc
Emanuel Marcos <105237236+ebarbozadev@users.noreply.github.com>
2023-05-30 20:00:52 -0300

    Colocando console.log

--- src/routes/Inicio.jsx
+++ src/routes/Inicio.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Tabela from '../componentes/Tabela';
+import pegarAPI from '../axios/config.js';
 
 export default function Inicio(){
+    console.log(pegarAPI);
+
     return(
         <div>
             <p>Inicio</p>
             <Tabela />
+
         </div>
     );
 }
