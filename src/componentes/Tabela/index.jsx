7225b376a5cae91b3261119909b7a37bdf136e59
Emanuel Marcos dos Santos Barboza <e.barboza@edu.unipar.br>
2023-06-01 11:38:04 -0300

    Acrescentando delet

--- src/componentes/Tabela/index.jsx
+++ src/componentes/Tabela/index.jsx
@@ -1,17 +1,46 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import styles from './Tabela.module.css';
+import axios from 'axios';
 
-export default function Tabela({ usuarios }) {
-    if (!Array.isArray(usuarios) || usuarios.length === 0) {
-        return <p>Não há usuários cadastrados</p>;
-    }
+export default function Tabela() {
+    const [usuarios, setUsuarios] = useState([]);
+
+    useEffect(() => {
+        const pegarUsuarios = async () => {
+            try {
+                const resposta = await axios.get(
+                    'http://servidorlocal.gerencesistemas.com.br:253/candidatos'
+                );
+                const data = resposta.data;
+                setUsuarios(data);
+                console.log(data); // Verifique os dados retornados pela API aqui
+            } catch (erro) {
+                console.log(erro);
+            }
+        };
+
+        pegarUsuarios();
+    }, []);
+
+    const excluirUsuario = async (codCandidato) => {
+        try {
+            await axios.delete(
+                'http://servidorlocal.gerencesistemas.com.br:253/candidatos',
+                { headers: { codCandidato } }
+            );
+            // Atualize a lista de usuários após a exclusão
+            const novaLista = usuarios.filter((usuario) => usuario.codCandidato !== codCandidato);
+            setUsuarios(novaLista);
+        } catch (erro) {
+            console.log(erro);
+        }
+    };
 
     return (
         <table>
             <thead>
                 <tr>
-                    <th>Código</th>
+                    <th>ID</th>
                     <th>Nome</th>
                     <th>CPF</th>
                     <th>Telefone</th>
@@ -25,18 +54,23 @@ export default function Tabela({ usuarios }) {
             <tbody>
                 {usuarios.map((usuario) => (
                     <tr key={usuario.codCandidato}>
-                        <td>{usuario.codCandidato}</td>
+                        <td className={styles.codCandidato}>{usuario.codCandidato}</td>
                         <td>{usuario.nomeCandidato}</td>
                         <td>{usuario.cpf}</td>
                         <td>{usuario.telefone}</td>
-                        <td>{usuario.Notas[0].nota01}</td>
-                        <td>{usuario.Notas[0].nota02}</td>
-                        <td>{usuario.Notas[0].nota03}</td>
-                        <td>10</td>
-                        <td>Aprovado</td>
-                        <td>
+                        {usuario.Notas.map((nota) => (
+                            <React.Fragment key={nota.idCandidato}>
+                                <td className={styles.nota01}>{nota.nota01}</td>
+                                <td className={styles.nota02}>{nota.nota02}</td>
+                                <td className={styles.nota03}>{nota.nota03}</td>
+                            </React.Fragment>
+                        ))}
+                        <td className={styles.estado}>Aprovado</td>
+                        <td className={styles.botoes}>
                             <span className={styles.alterar_btn}>Alterar</span>{' '}
-                            <span className={styles.excluir_btn}>Excluir</span>
+                            <span className={styles.excluir_btn} onClick={() => excluirUsuario(usuario.codCandidato)}>
+                                Excluir
+                            </span>
                         </td>
                     </tr>
                 ))}
--- src/routes/Inicio.jsx
+++ src/routes/Inicio.jsx
@@ -1,34 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-import Tabela from '../componentes/Tabela';
+import Tabela from '../componentes/Tabela'
 
 export default function Inicio() {
-    const [usuarios, setUsuarios] = useState([]);
-
-    useEffect(() => {
-        const pegarUsuarios = async () => {
-            try {
-                const resposta = await axios.get(
-                    'http://servidorlocal.gerencesistemas.com.br:253/candidatos'
-                );
-                const data = resposta.data;
-                setUsuarios(data);
-                console.log(data); // Verifique os dados retornados pela API aqui
-            } catch (erro) {
-                console.log(erro);
-            }
-        };
-
-        pegarUsuarios();
-    }, []);
-    return (
-        <div>
-            {usuarios.length === 0 ? (
-                <p>Não há usuários cadastrados</p>
-            ) : (
-                <Tabela usuarios={usuarios} />
-            )}
-        </div>
-    );
+    
 }
